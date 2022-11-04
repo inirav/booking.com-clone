2a79aaf5e9527af84ac718ae415e8d99e5f5d092
pkkulhari <praveen.kulhari07@gmail.com>
2022-11-04 10:19:29 +0530

    feat: add routes for properties and rooms

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,19 @@
 import App from './app'
 import AuthRoute from './routes/auth.route'
 import IndexRoute from './routes/index.route'
+import PropertiesRoute from './routes/properties.route'
+import RoomsRoute from './routes/rooms.route'
 import UsersRoute from './routes/users.route'
 import validateEnv from './utils/validateEnv'
 
 validateEnv()
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute()])
+const app = new App([
+  new IndexRoute(),
+  new AuthRoute(),
+  new UsersRoute(),
+  new PropertiesRoute(),
+  new RoomsRoute(),
+])
 
 app.listen()
