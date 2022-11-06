bebc2298da626e55bdb8bbe38b953ebe8f908806
pkkulhari <praveen.kulhari07@gmail.com>
2022-11-06 12:11:20 +0530

    refactor: rename hotel to property

--- admin/src/constants/HotelTypes.ts
+++
@@ -1,6 +0,0 @@
-const HotelTypes = [
-  { value: 'resort', label: 'Resort' },
-  { value: 'villa', label: 'Villa' },
-]
-
-export default HotelTypes
---
+++ admin/src/constants/propertyTypes.ts
@@ -0,0 +1,10 @@
+enum PropertyTypes {
+  HOTEL = 'hotel',
+  VILLA = 'villa',
+  APARTMENT = 'apartment',
+  RESORT = 'resort',
+  CABIN = 'cabin',
+  COTTAGE = 'cottage',
+}
+
+export default PropertyTypes
