afd2fdc5f84744ee8922e4594391c0cfd16b8290
pkkulhari <praveen.kulhari07@gmail.com>
2022-11-06 12:14:35 +0530

    feat: implement count in cards

--- admin/src/pages/Home/index.tsx
+++ admin/src/pages/Home/index.tsx
@@ -1,18 +1,28 @@
+import { useQuery } from '@tanstack/react-query'
 import Card from '../../components/Card'
+import { getPropertyCount } from '../../services/properties'
+import { getUserCount } from '../../services/users'
 import { Cards } from './styles'
 
 type Props = {}
 
 const Home = (props: Props) => {
+  const { data: userCount, isSuccess: isSuccessUserCount } = useQuery(['userCount'], getUserCount)
+  const { data: propertyCount, isSuccess: isSuccessPropertyCount } = useQuery(
+    ['propertyCount'],
+    getPropertyCount
+  )
+
+  console.log(propertyCount)
+
   return (
-    <>
-      <Cards>
-        <Card title="Users" count={40} path="/users" />
-        <Card title="Hotels" count={40} path="/hotels" />
-        <Card title="Reservation" count={40} path="/reservations" />
-        <Card title="Rooms" count={40} />
-      </Cards>
-    </>
+    <Cards>
+      {isSuccessUserCount && <Card title="Users" count={userCount} path="/users" />}
+      {isSuccessPropertyCount && (
+        <Card title="Properties" count={propertyCount} path="/properties" />
+      )}
+      <Card title="Reservation" count={40} path="/reservations" />
+    </Cards>
   )
 }
 
