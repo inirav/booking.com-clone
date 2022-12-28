4f379304aa86be84aa4086ff12842042884d40f6
pkkulhari <praveen.kulhari07@gmail.com>
2022-12-28 10:15:54 +0530

    fix: add production api base endpoint

--- admin/src/utils/api.ts
+++ admin/src/utils/api.ts
@@ -2,7 +2,10 @@ import axios from 'axios'
 import queryString from 'query-string'
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:5000',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:5000'
+      : 'https://booking-com-api.onrender.com',
   paramsSerializer: queryString.stringify,
 })
 
