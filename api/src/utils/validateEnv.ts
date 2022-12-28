6a36f86d03c44dfe38dc3e1768cf4559923fee68
pkkulhari <praveen.kulhari07@gmail.com>
2022-12-28 09:14:28 +0530

    fix: validate env

--- api/src/utils/validateEnv.ts
+++ api/src/utils/validateEnv.ts
@@ -2,9 +2,6 @@ import { cleanEnv, port, str } from 'envalid'
 
 const validateEnv = () => {
   cleanEnv(process.env, {
-    DB_HOST: str(),
-    DB_PORT: port(),
-    DB_NAME: str(),
     JWT_SECRET: str(),
     CLOUDINARY_CLOUD_NAME: str(),
     CLOUDINARY_API_KEY: str(),
